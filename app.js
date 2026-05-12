aaed560c1b9fb1fc4449528154a829bb5ae60bdf
lightspot-coder <214618438+lightspot-coder@users.noreply.github.com>
2026-05-12 17:28:57 +0200

    adding some styles

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const indexRouter = require("./routes/indexRouter");
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
